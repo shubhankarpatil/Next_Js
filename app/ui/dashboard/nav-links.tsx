f67d8bc7c55e4b2a8c243700e49a1170e5b6958b
shubhankarpatil <shubhankar.a.patil@gmail.com>
2024-07-14 20:56:23 +0530

    Temporart changes

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -27,7 +27,7 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
+          <a
             key={link.name}
             href={link.href}
             className={clsx(
@@ -39,7 +39,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </Link>
+          </a>
         );
       })}
     </>
